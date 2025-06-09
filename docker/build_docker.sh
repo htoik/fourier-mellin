7e028b09badc4c69058e4ac26217cdc86cc96104
htoik <henrik.toikka@aalto.fi>
2025-06-09 13:39:13 +0300

    removed docker and push scripts for now

--- docker/build_docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# manylinux-x86_64
-docker build -f Dockerfile.manylinux_x86_64 -t ghcr.io/htoik/fourier-mellin/manylinux_x86_64:latest --load .
-# manylinux-aarch64
-docker buildx build --platform linux/arm64 -f Dockerfile.manylinux_aarch64 -t ghcr.io/htoik/fourier-mellin/manylinux_aarch64:latest --load .
